05c8d683e2ef186e8f5c8acecb4158bd666ff384
pablo.lucas <pablo.lucv@gmail.com>
2023-07-06 19:02:00 -0600

    Added producer callback

--- spring-kafka-ms/kafka-ms-order/src/main/java/com/plucas/kafka/broker/producer/OrderProducer.java
+++ spring-kafka-ms/kafka-ms-order/src/main/java/com/plucas/kafka/broker/producer/OrderProducer.java
@@ -1,11 +1,18 @@
 package com.plucas.kafka.broker.producer;
 
 import com.plucas.kafka.broker.message.OrderMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class OrderProducer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OrderProducer.class);
 
     private KafkaTemplate<String, OrderMessage> kafkaTemplate;
 
@@ -14,6 +21,15 @@ public class OrderProducer {
     }
 
     public void publish(OrderMessage message) {
-        kafkaTemplate.send("t-commodity-order", message.getOrderNumber(), message);
+        CompletableFuture<SendResult<String, OrderMessage>> future = kafkaTemplate.send("t-commodity-order", message.getOrderNumber(), message);
+        future.whenCompleteAsync((result, ex) -> {
+            if (ex == null) {
+                LOG.info("Order {}, item {} published successfully", message.getOrderNumber(), message.getItemName());
+            } else {
+                LOG.info("Order {}, item {} failed to publish because {}", message.getOrderNumber(), message.getItemName(), ex.getMessage());
+            }
+        });
+
+        LOG.info("Just a dummy message for order {}, item {}", message.getOrderNumber(), message.getItemName());
     }
 }
